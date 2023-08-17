98c40607d3ea5cfd6b6708a2eddbc6f3b200d586
Aegor1998 <50035405+Aegor1998@users.noreply.github.com>
2023-08-17 15:17:28 -0700

    Create personalSetUp.cmd
    In Progress

---
+++ personalSetUp.cmd
@@ -0,0 +1,16 @@
+# Author: Aegor 1998  Created: 08/17/2023
+# This is a script to download stuff through winget
+
+# Install winget - https://learn.microsoft.com/en-us/windows/package-manager/winget/
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+  $officePrograms = @('Mozilla.Firefox'
+  
+  )
