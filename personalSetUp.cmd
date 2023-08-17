efe5d7a257c2810d4dc2aa89352285439b40f862
Aegor1998 <50035405+Aegor1998@users.noreply.github.com>
2023-08-17 15:58:24 -0700

    Update personalSetUp.cmd
    Work to do
    - add ameliorated section
    - add install section
    - reset Set-ExecutionPolicy
    - test

--- personalSetUp.cmd
+++ personalSetUp.cmd
@@ -1,16 +1,35 @@
 # Author: Aegor 1998  Created: 08/17/2023
-# This is a script to download stuff through winget
+# Full Windows set up script
 
-# Install winget - https://learn.microsoft.com/en-us/windows/package-manager/winget/
-$progressPreference = 'silentlyContinue'
-Write-Information "Downloading WinGet and its dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+# Making it so all scripts can run
+Set-ExecutionPolicy Bypass -Scope Process
+# NEED TO ADD A LINE TO TURN THIS OFF AT THE END
 
-  $officePrograms = @('Mozilla.Firefox'
-  
-  )
+# need to create a section to run ameliorated (ameliorated.io)
+  # Check version of windows
+  # Download ameliorated
+  # Download playbooks
+    # Make sure to check which AME playbook to download
+
+#Chocolatey Install Script - https://chocolatey.org/install
+Set-ExecutionPolicy Bypass -Scope Process -Force; \
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; \
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Program Lists
+$programs = @('firefox'
+              'thunderbird'
+              'birdtray'
+              'libreoffice-fresh'
+              'steam'
+              'epicgameslauncher'
+              'goggalaxy'
+              '7zip.install'
+              'bitwarden'
+              'clamav'
+              'picotorrent'
+              'rustdesk'
+)
+
+# Install Loop Statement
+# format: choco install program
