875e9f98fc4c2570361ce07eb2b2b543a38abdcc
Aegor1998 <50035405+Aegor1998@users.noreply.github.com>
2023-08-17 14:58:06 -0700

    Update bsodQuickFix.cmd
    Version 2
    - added gpupdate
    - changed the DISM command
    - added a reboot

--- bsodQuickFix.cmd
+++ bsodQuickFix.cmd
@@ -1,10 +1,12 @@
-# This script will run through some common fixes for BSOD
-# This 
+# Author: Aegor1998  Created: 08/17/2023
+# This script will check for corrupt files on a system
 
+# Running this because where DISM pulls from is controlled
+# by Group Policy
+gpupdate /force
 # Updates the what sfc will check against
 DISM.exe /Online /Cleanup-image /Restorehealth
 # Does the actual repair
 sfc /scannow
-# This line keeps the terminal open
-cmd /k
+# Reboot Computer
 shutdown /r
