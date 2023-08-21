d40ddac21e614b2b27d8e9dc93e73c6522b5133a
Aegor1998 <50035405+Aegor1998@users.noreply.github.com>
2023-08-21 16:40:51 -0700

    Update powerReports.cmd
    moved the battery section to the bottom

--- powerReports.cmd
+++ powerReports.cmd
@@ -4,12 +4,12 @@
 #Creating the folder in which all of the reports will be found
 mkdir c:\powercfgReports
 
-#Will not run if there is no battery
-powercfg /batteryreport
-move C:\windows\system32\battery-report.html c:\powercfgReports\battery-report.html
-
 powercfg /energy
 move C:\windows\system32\energy-report.html c:\powercfgReports\energy-report.html
   
 powercfg /systempowerreport
 move C:\windows\system32\sleepstudy-report.html c:\powercfgReports\sleepstudy-report.html
+
+#Will not run if there is no battery
+powercfg /batteryreport
+move C:\windows\system32\battery-report.html c:\powercfgReports\battery-report.html
