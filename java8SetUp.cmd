7ca146a05aac6aefb903441a8eedcc55f338fa02
Aegor1998 <50035405+Aegor1998@users.noreply.github.com>
2023-09-22 13:56:51 -0700

    Update java8SetUp.cmd

--- java8SetUp.cmd
+++ java8SetUp.cmd
@@ -1,10 +1,8 @@
 # Author: Aegor1998   Created: 9/21/2023
-# Updated: Sepetember 22nd, 2023 1348PST
+# Last Updated: Sepetember 22nd, 2023
 # Download and set-up Java 8 using winget (must be run as admin)
 # [x] Stage 1: Check if winget is installed, install winget, download java
 # [x] Stage 2: Add websites from a list to the security white list
-# [x] Stage 3: Check for "deploymentruleset.jar" and remove if found.
-# [x] Stage 4: Remove Java before installing through winget
 
 # Will not run if started with elevated privlages over the logged in account
 # Clears all Variables
@@ -23,21 +21,15 @@ if(Get-AppxPackage -AllUsers | Select-String "Microsoft.Winget.Source"){
   Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
   Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 }
-#Check if Java is installed
 
 #Install Java
-if(winget list | Select-String "potato"){
+if(winget list | Select-String "Java 8"){
 winget install --id=Oracle.JavaRuntimeEnvironment -e
 }
 
 #Exceptions List
 $site = "https://www.test.com"
 Add-Content -Path "$env:USERPROFILE\AppData\LocalLow\Sun\Java\Deployment\security\exception.sites" -Value "$site"
-
-# Check for "deploymentruleset.jar" and remove if found
-if (Test-Path C:\Windows\Sun){
-    powershell -windowstyle hidden -command Remove-Item C:\Windows\sun
-}
 
 # Sign off
 Write-Host "Java 8 has been installed and configured"
